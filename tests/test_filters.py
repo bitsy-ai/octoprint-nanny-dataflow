f79a86b39e187a5abfdd976e52616d91498a3ee1
Leigh Johnson <hi@leighjohnson.me>
2021-03-20 13:52:29 -0700

    add 3 passing tests

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -32,74 +32,90 @@ def test_area_of_intersection_overlap(partial_nested_telemetry_event_kwargs):
     np.testing.assert_almost_equal(percent_area[0], expected)
 
 
-# def test_area_of_intersection_no_overlap_0():
-#     predictor = ThreadLocalPredictor()
-#     detection_boxes = np.array([[0.3, 0.3, 0.9, 0.9]])
+def test_area_of_intersection_no_overlap_0(partial_nested_telemetry_event_kwargs):
 
-#     calibration_box = [0.1, 0.1, 0.2, 0.2]
+    detection_boxes = np.array([[0.3, 0.3, 0.9, 0.9]])
+    boxes_ymin, boxes_xmin, boxes_ymax, boxes_xmax = detection_boxes.T
 
-#     detection_scores = np.array([1])
+    calibration_box = [0.1, 0.1, 0.2, 0.2]
 
-#     detection_classes = np.array([4])
+    detection_scores = np.array([1])
 
-#     prediction = octoprint_nanny.types.BoundingBoxPrediction(
-#         detection_boxes=detection_boxes,
-#         detection_scores=detection_scores,
-#         detection_classes=detection_classes,
-#         num_detections=len(detection_classes),
-#     )
-
-#     percent_area = predictor.percent_intersection(prediction, calibration_box)
-#     expected = 0.0
-#     np.testing.assert_almost_equal(percent_area[0], expected)
+    detection_classes = np.array([4])
 
 
-# def test_area_of_intersection_no_overlap_1():
-#     predictor = ThreadLocalPredictor()
-#     detection_boxes = np.array([[0.5, 0.2, 0.9, 0.4]])
+    event = NestedTelemetryEvent(
+        boxes_ymin=boxes_ymin,
+        boxes_xmin=boxes_xmin,
+        boxes_ymax=boxes_ymax,
+        boxes_xmax=boxes_xmax,
+        detection_classes=detection_classes,
+        detection_scores=detection_scores,
+        **partial_nested_telemetry_event_kwargs,
 
-#     calibration_box = [0.1, 0.7, 0.39, 0.8]
+    )
 
-#     detection_scores = np.array([1])
-
-#     detection_classes = np.array([4])
-
-#     prediction = octoprint_nanny.types.BoundingBoxPrediction(
-#         detection_boxes=detection_boxes,
-#         detection_classes=detection_classes,
-#         detection_scores=detection_scores,
-#         num_detections=len(detection_classes),
-#     )
-
-#     percent_area = predictor.percent_intersection(prediction, calibration_box)
-#     expected = 0.0
-#     np.testing.assert_almost_equal(percent_area[0], expected)
+    percent_area = event.percent_intersection(detection_boxes, calibration_box)
+    expected = 0.0
+    np.testing.assert_almost_equal(percent_area[0], expected)
 
 
-# def test_area_of_intersection_prediction_contained_0():
+def test_area_of_intersection_no_overlap_1(partial_nested_telemetry_event_kwargs):
+    detection_boxes = np.array([[0.5, 0.2, 0.9, 0.4]])
+    boxes_ymin, boxes_xmin, boxes_ymax, boxes_xmax = detection_boxes.T
 
-#     predictor = ThreadLocalPredictor()
-#     detection_boxes = np.array([[0.2, 0.2, 0.8, 0.8]])
+    calibration_box = [0.1, 0.7, 0.39, 0.8]
 
-#     calibration_box = [0.1, 0.1, 0.9, 0.9]
+    detection_scores = np.array([1])
 
-#     detection_scores = np.array([1])
+    detection_classes = np.array([4])
 
-#     detection_classes = np.array([4])
+    event = NestedTelemetryEvent(
+        boxes_ymin=boxes_ymin,
+        boxes_xmin=boxes_xmin,
+        boxes_ymax=boxes_ymax,
+        boxes_xmax=boxes_xmax,
+        detection_classes=detection_classes,
+        detection_scores=detection_scores,
+        **partial_nested_telemetry_event_kwargs,
 
-#     prediction = octoprint_nanny.types.BoundingBoxPrediction(
-#         detection_boxes=detection_boxes,
-#         detection_classes=detection_classes,
-#         detection_scores=detection_scores,
-#         num_detections=len(detection_classes),
-#     )
-
-#     percent_area = predictor.percent_intersection(prediction, calibration_box)
-#     expected = 1.0
-#     np.testing.assert_almost_equal(percent_area[0], expected)
+    )
 
 
-# def test_print_health_trend_increasing():
+    percent_area = event.percent_intersection(detection_boxes, calibration_box)
+    expected = 0.0
+    np.testing.assert_almost_equal(percent_area[0], expected)
+
+
+def test_area_of_intersection_prediction_contained_0(partial_nested_telemetry_event_kwargs):
+
+    detection_boxes = np.array([[0.2, 0.2, 0.8, 0.8]])
+    boxes_ymin, boxes_xmin, boxes_ymax, boxes_xmax = detection_boxes.T
+
+    calibration_box = [0.1, 0.1, 0.9, 0.9]
+
+    detection_scores = np.array([1])
+
+    detection_classes = np.array([4])
+
+    event = NestedTelemetryEvent(
+        boxes_ymin=boxes_ymin,
+        boxes_xmin=boxes_xmin,
+        boxes_ymax=boxes_ymax,
+        boxes_xmax=boxes_xmax,
+        detection_classes=detection_classes,
+        detection_scores=detection_scores,
+        **partial_nested_telemetry_event_kwargs,
+
+    )
+
+
+    percent_area = event.percent_intersection(detection_boxes, calibration_box)
+    expected = 1.0
+    np.testing.assert_almost_equal(percent_area[0], expected)
+
+
+# def test_print_health_trend_increasing(partial_nested_telemetry_event_kwargs):
 #     num_detections = 40
 #     prediction1 = octoprint_nanny.types.BoundingBoxPrediction(
 #         detection_classes=np.repeat(4, num_detections),
