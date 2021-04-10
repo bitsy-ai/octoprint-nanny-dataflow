cf6fa17fe0d5cbf1158de6c5d3a5354ff550a96e
Leigh Johnson <hi@leighjohnson.me>
2021-04-10 09:45:30 -0700

    add env files

--- prod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PROJECT=print-nanny
-export REPO=print-nanny-dataflow
-export TAG=$(git rev-parse HEAD)
-export REGISTRY_HOST=gcr.io
-export IMAGE_URI="$REGISTRY_HOST/$PROJECT/$REPO:$TAG"
--- sandbox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PROJECT=print-nanny-sandbox
-export REPO=print-nanny-dataflow
-export TAG=$(git rev-parse HEAD)
-export REGISTRY_HOST=gcr.io
-export IMAGE_URI="$REGISTRY_HOST/$PROJECT/$REPO:$TAG"
-gcloud builds submit --tag $IMAGE_URI
